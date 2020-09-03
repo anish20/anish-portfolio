70dac3052358f558918b8867379a2c25828f32dd
Mohd Anish <anish15.786@gmail.com>
2020-09-03 21:42:28 +0530

    first time uploaded

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+// Navbar
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true,
+        
+      });
+      
+      $('.materialboxed').materialbox();
+
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'right'
+    });
+      
+      $('.tooltipped').tooltip();
+
+      $(".scroll").click(function(event){		
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+    });
+
+    $().UItoTop({ easingType: 'easeOutQuart' });
+   
+
+  });
+
+  
